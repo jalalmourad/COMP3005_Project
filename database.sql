901d4b4cc8c8001983bd643326ab3b9b7560c961
Owen Hobbs <104236187+Fatality150@users.noreply.github.com>
2024-03-25 20:14:11 -0400

    Update and rename tables.sql to database.sql
    finishing touches on the database sql file

--- database.sql
+++ database.sql
@@ -1,19 +1,20 @@
 create table goals
-    (
-        goal_id SERIAL,
-        member_id int,
-        goal_description text,
-        achieve_by_date date default current_date,
-        start_weight int,
-        goal_weight int,
-        start_time int,
-        goal_time int,
-        is_achieved boolean default false,
-        primary key (goal_id),
-        foreign key (member_id) REFERENCES members
-    )
+(
+    goal_id SERIAL,
+    member_id int,
+    goal_description text,
+    achieve_by_date date default current_date,
+    start_weight int,
+    goal_weight int,
+    start_time int,
+    goal_time int,
+    is_achieved boolean default false,
+    primary key (goal_id),
+    foreign key (member_id) REFERENCES members
+);
 
-CREATE TABLE Trainers(
+CREATE TABLE Trainers
+(
     --trainer info
     trainer_id SERIAL,
     first_name varchar(255) not null,
@@ -23,7 +24,8 @@ CREATE TABLE Trainers(
 );
 
 -- administrative staff entity 
-CREATE TABLE Administrative_Staff(
+CREATE TABLE Administrative_Staff
+(
     admin_id SERIAL,
     first_name varchar(255),
     last_name varchar(255),
@@ -31,12 +33,12 @@ CREATE TABLE Administrative_Staff(
     schedule_id int REFERENCES Schedule(schedule_id)
 );
 
-CREATE TABLE Equipment(
-
-equipment_id SERIAL,
-equipment_name text,
-equipment_maintenance_date Date,
-primary key (equipment_id)
+CREATE TABLE Equipment
+(
+    equipment_id SERIAL,
+    equipment_name text,
+    equipment_maintenance_date Date,
+    primary key (equipment_id)
 );
 
 create table member_booking
@@ -53,7 +55,9 @@ CREATE TABLE billing
     bill_date date default current_date,
     foreign key (member_id) references members
 );
-CREATE TABLE Schedule(
+
+CREATE TABLE Schedule
+(
     schedule_id SERIAL,
     trainer_id int,
     appointment_time time not null,
@@ -64,20 +68,21 @@ CREATE TABLE Schedule(
     primary key (schedule_id),
     foreign key (trainer_id) references trainers
 );
+
 create table members
-    (
-        --member info
-        member_id SERIAL,
-        first_name varchar(255) not null,
-        last_name varchar(255) not null,
-        -- billing info
-        date_joined date default current_date,
-        fee int default 100,
-        -- health metrics
-        member_weight int default 0,
-        member_height int default 0,
-        member_time int default 0,
-        --
-        track_exercise_routine text not null,
-        primary key (member_id)
-    );
+(
+    --member info
+    member_id SERIAL,
+    first_name varchar(255) not null,
+    last_name varchar(255) not null,
+    -- billing info
+    date_joined date default current_date,
+    fee int default 100,
+    -- health metrics
+    member_weight int default 0,
+    member_height int default 0,
+    member_time int default 0,
+    --
+    track_exercise_routine text not null,
+    primary key (member_id)
+);
