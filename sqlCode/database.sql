685779f7abd270dea737302ca69b492e73c188eb
Owen Hobbs <104236187+Fatality150@users.noreply.github.com>
2024-03-27 17:45:01 -0400

    Update database.sql
    fixing db order

--- sqlCode/database.sql
+++ sqlCode/database.sql
@@ -1,75 +1,3 @@
-create table goals
-(
-    goal_id SERIAL,
-    member_id int,
-    goal_description text,
-    achieve_by_date date default current_date,
-    start_weight int,
-    goal_weight int,
-    start_time int,
-    goal_time int,
-    is_achieved boolean default false,
-    primary key (goal_id),
-    foreign key (member_id) REFERENCES members
-);
-
-CREATE TABLE Trainers
-(
-    --trainer info
-    trainer_id SERIAL,
-    first_name varchar(255) not null,
-    last_name varchar(255) not null,
-    salary int default 3000,
-    primary key (trainer_id)
-);
-
--- administrative staff entity 
-CREATE TABLE Administrative_Staff
-(
-    admin_id SERIAL,
-    first_name varchar(255),
-    last_name varchar(255),
-    -- foreign key referencing schedule_id in schedule entity 
-    schedule_id int REFERENCES Schedule(schedule_id)
-);
-
-CREATE TABLE Equipment
-(
-    equipment_id SERIAL,
-    equipment_name text,
-    equipment_maintenance_date Date,
-    admin_id int REFERENCES Administrative_Staff(admin_id),
-    primary key (equipment_id)
-);
-
-create table member_booking
-(
-    member_id   integer not null references members(member_id),
-    schedule_id   integer not null references schedule(schedule_id)
-);
-
-CREATE TABLE billing
-(
-    member_id int,
-    amount int not null,
-    schedule_id int, 
-    bill_date date default current_date,
-    foreign key (member_id) references members
-);
-
-CREATE TABLE Schedule
-(
-    schedule_id SERIAL,
-    trainer_id int,
-    appointment_time time not null,
-    appointment_date date not null,
-    appointment_room int not null,
-    joining_fee int default 10,
-    max_members int default 1,
-    primary key (schedule_id),
-    foreign key (trainer_id) references trainers
-);
-
 create table members
 (
     --member info
@@ -86,4 +14,77 @@ create table members
     --
     track_exercise_routine text not null,
     primary key (member_id)
+);
+
+CREATE TABLE Trainers
+(
+    --trainer info
+    trainer_id SERIAL,
+    first_name varchar(255) not null,
+    last_name varchar(255) not null,
+    salary int default 3000,
+    primary key (trainer_id)
+);
+
+CREATE TABLE Schedule
+(
+    schedule_id SERIAL,
+    trainer_id int,
+    appointment_time time not null,
+    appointment_date date not null,
+    appointment_room int not null,
+    joining_fee int default 10,
+    max_members int default 1,
+    primary key (schedule_id),
+    foreign key (trainer_id) references trainers
+);
+
+create table goals
+(
+    goal_id SERIAL,
+    member_id int,
+    goal_description text,
+    achieve_by_date date default current_date,
+    start_weight int,
+    goal_weight int,
+    start_time int,
+    goal_time int,
+    is_achieved boolean default false,
+    primary key (goal_id),
+    foreign key (member_id) REFERENCES members
+);
+
+
+
+-- administrative staff entity 
+CREATE TABLE Administrative_Staff
+(
+    admin_id SERIAL,
+    first_name varchar(255),
+    last_name varchar(255),
+    -- foreign key referencing schedule_id in schedule entity 
+    schedule_id int REFERENCES Schedule(schedule_id)
+);
+
+CREATE TABLE Equipment
+(
+    equipment_id SERIAL,
+    equipment_name text,
+    equipment_maintenance_date Date,
+    primary key (equipment_id)
+);
+
+create table member_booking
+(
+    member_id   integer not null references members(member_id),
+    schedule_id   integer not null references schedule(schedule_id)
+);
+
+CREATE TABLE billing
+(
+    member_id int,
+    amount int not null,
+    schedule_id int, 
+    bill_date date default current_date,
+    foreign key (member_id) references members
 );
